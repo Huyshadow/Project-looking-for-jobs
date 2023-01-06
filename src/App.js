8c47251b92c481c77f57ac2ae14c7ec50ee80d6f
Huy Dang <huy.dang110902@gmail.com>
2023-01-06 12:19:39 +0700

    CheckFrontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css"
+import Home from './components/Home';
+import { BrowserRouter, NavLink, Link, Route, Routes } from "react-router-dom";
+import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
+import Course from "./components/Courses/Course";
+import Login from "./components/LoginSignin/Login";
+import {AiOutlineUnorderedList} from "react-icons/ai"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <div className={styles.navbar}>
+        <Link to="/" className={styles.link}><h4>HUYLEARN</h4></Link>
+        <ul className={styles.nav_menu}>
+          <Link to='/about' className={styles.link}>About</Link>
+          <Link to='/course' className={styles.link}>Course</Link>
+          <Link to='/contact' className={styles.link}>Contact</Link>
+        </ul>
+        <Link to='/login' className={styles.link}>Login</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="course" element={<Course />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
