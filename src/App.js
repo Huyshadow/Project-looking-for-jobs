6c0b5c739aedc797b4714bc76409b173171f8ebc
Huy Dang <huy.dang110902@gmail.com>
2023-01-07 08:33:51 +0700

    Root css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import styles from "./App.module.css"
 import Home from './components/Home';
-import { BrowserRouter, NavLink, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Contact from "./components/Contact/Contact";
 import About from "./components/About/About";
 import Course from "./components/Courses/Course";
 import Login from "./components/LoginSignin/Login";
-import {AiOutlineUnorderedList} from "react-icons/ai"
+import {AiOutlineUnorderedList, AiOutlineCloseCircle} from "react-icons/ai"
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <div className={styles.navbar}>
-        <Link to="/" className={styles.link}><h4>HUYLEARN</h4></Link>
-        <ul className={styles.nav_menu}>
-          <Link to='/about' className={styles.link}>About</Link>
-          <Link to='/course' className={styles.link}>Course</Link>
-          <Link to='/contact' className={styles.link}>Contact</Link>
-        </ul>
-        <Link to='/login' className={styles.link}>Login</Link>
+        <div className={styles.container}>
+        <div className={styles.navbar}>
+          <Link to="/" className={styles.link}><h4>HUYLEARN</h4></Link>
+          <ul className={styles.nav_menu}>
+            <Link to='/about' className={styles.link}>About</Link>
+            <Link to='/course' className={styles.link}>Course</Link>
+            <Link to='/contact' className={styles.link}>Contact</Link>
+          </ul>
+            <Link to='/login' className={styles.link}>Login</Link>
+          <div>
+            <AiOutlineUnorderedList />
+            <AiOutlineCloseCircle />
+          </div>
         </div>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -26,7 +31,8 @@ function App() {
           <Route path="course" element={<Course />} />
           <Route path="contact" element={<Contact />} />
           <Route path="login" element={<Login />} />
-        </Routes>
+          </Routes>
+        </div>
       </BrowserRouter>
     </>
   );
