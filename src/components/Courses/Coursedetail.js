72174e86f80a53766eb095f8d54fd29da6315f3e
Huy Dang <huy.dang110902@gmail.com>
2023-01-12 15:20:35 +0700

    Some fix in course

--- src/components/Courses/Coursedetail.js
+++ src/components/Courses/Coursedetail.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import styles from "../../css/Coursedetail.module.css";
 const CourseDetail = ({ title, categories, imgUrl }) => {
+  const navigate = useNavigate();
+  function navigatetoDetail() {
+    navigate("/course/ele");
+  }
   return (
     <div className={styles.course}>
       <img src={imgUrl} />
       <div className={styles.txtx}>
         <h4>{title}</h4>
         <span>{categories}</span>
+        <button className={styles.btn}>Detail</button>
       </div>
     </div>
   );
