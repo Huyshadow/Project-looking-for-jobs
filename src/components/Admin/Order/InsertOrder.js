73579570006252dcb867e653d3321f9a21cbdd7b
Huy Dang <huy.dang110902@gmail.com>
2023-01-18 16:54:34 +0700

    Admin page Order

---
+++ src/components/Admin/Order/InsertOrder.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../../../css/InsertOrder.module.css";
+const InsertOrder = () => {
+  return <></>;
+};
+export default InsertOrder;
--- src/components/Admin/Order/Order.js
+++ src/components/Admin/Order/Order.js
@@ -1,6 +1,46 @@
 import React from "react";
-
+import styles from "../../../css/Order.module.css";
 const Order = () => {
-  return <>Order</>;
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Order</h1>
+        <div className={styles.note}>
+          <p>
+            <i>Last updated 24h ago</i>
+          </p>
+        </div>
+        <main>
+          <table>
+            <thead>
+              <tr>
+                <td>OrderID</td>
+                <td>UserID</td>
+                <td>Status</td>
+                <td>Manager ID</td>
+                <td>Warehouse Code</td>
+                <td>Transportation Code</td>
+                <td></td>
+              </tr>
+            </thead>
+            {/*listReport.map((report) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td>{report.ID}</td>
+                    <td>{report.Ngay_xuat_bien_ban}</td>
+                    <td>{report.Tinh_trang}</td>
+                    <td>{report.ma_nv_quan_ly_kho}</td>
+                    <td>{report.ma_kho}</td>
+                    <td>{report.ma_chuyen}</td>
+                  </tr>
+                </tbody>
+              );
+            })*/}
+          </table>
+        </main>
+      </div>
+    </>
+  );
 };
 export default Order;
